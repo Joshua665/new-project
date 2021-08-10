ec93ee0039296ad0c282f5abf26e666a5eb8febb
Joshua665 <88707737+Joshua665@users.noreply.github.com>
2021-08-09 20:27:13 -1200

    Add files via upload

---
+++ myfirstapp/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
+# Create your views here.
+
+def myfunctioncall(request):
+    return HttpResponse("Hello World!")
+
+
+def myfunctionabout(request):
+    return HttpResponse("About Response")    
+
+def add(request, a, b):
+    return HttpResponse(a+b)
+def intro(request,name, age):
+    mydictionary ={
+        "name" : name,
+        "age" : age
+    }
+    return JsonResponse(mydictionary)    
+
+def myfirstpage(request):
+    return render(request,'index.html')    
+
+def mysecondpage(request):
+    return render(request, 'second.html')
+
+def mythirdpage(request):
+    var = "Hello World!"
+    greeting = "Hello how are you?"
+    fruits = ["apple", "banana", "orange"]
+    num1 , num2 = 3 , 5
+    ans = num1 > num2
+    #print(ans)
+    mydictionary = {
+        "var": var,
+        "msg": greeting,
+        "myfruits": fruits,
+        "num1": num1,
+        "num2": num2,
+        "ans": ans
+        
+    }
+    return render(request, 'third.html', context=mydictionary)
+
+def imagepage(request):
+    return render (request, 'imagepage.html')    
+
+def imagepage2(request):
+    return render (request, 'imagepage2.html')    
+
+def imagepage3(request):
+    return render (request, 'imagepage3.html')  
+
+def imagepage4(request):
+    return render (request, 'imagepage4.html')              
