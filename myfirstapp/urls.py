17738e13a0df0783ebf2f12511782f8dc0291c86
West Joshua Dein <west7828@run.edu.ng>
2021-08-11 20:41:12 -1200

    added new HTML page

--- myfirstapp/urls.py
+++ myfirstapp/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('imagepage2',views.imagepage2,name = "imagepage2"),
     path('imagepage3',views.imagepage3,name = "imagepage3"),
     path('imagepage4',views.imagepage4,name = "imagepage4"),
-    path('imagepage5/<str:imagename>',views.imagepage5,name = "imagepage5")
+    path('imagepage5/<str:imagename>',views.imagepage5,name = "imagepage5"),
+    path('myform',views.myform,name = "myform")
 
 ]
