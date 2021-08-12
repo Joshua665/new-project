986d02fb6da440197f8e59bfccabb7ff00aa7e9a
West Joshua Dein <west7828@run.edu.ng>
2021-08-12 00:55:55 -1200

    constantly learning new things!!

--- myfirstapp/views.py
+++ myfirstapp/views.py
@@ -3,35 +3,42 @@ from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 
+
 def myfunctioncall(request):
     return HttpResponse("Hello World!")
 
 
 def myfunctionabout(request):
-    return HttpResponse("About Response")    
+    return HttpResponse("About Response")
+
 
 def add(request, a, b):
     return HttpResponse(a+b)
-def intro(request,name, age):
-    mydictionary ={
-        "name" : name,
-        "age" : age
+
+
+def intro(request, name, age):
+    mydictionary = {
+        "name": name,
+        "age": age
     }
-    return JsonResponse(mydictionary)    
+    return JsonResponse(mydictionary)
+
 
 def myfirstpage(request):
-    return render(request,'index.html')    
+    return render(request, 'index.html')
+
 
 def mysecondpage(request):
     return render(request, 'second.html')
+
 
 def mythirdpage(request):
     var = "Hello World!"
     greeting = "Hello how are you?"
     fruits = ["apple", "banana", "orange"]
-    num1 , num2 = 3 , 5
+    num1, num2 = 3, 5
     ans = num1 > num2
-    #print(ans)
+    # print(ans)
     mydictionary = {
         "var": var,
         "msg": greeting,
@@ -39,21 +46,26 @@ def mythirdpage(request):
         "num1": num1,
         "num2": num2,
         "ans": ans
-        
+
     }
     return render(request, 'third.html', context=mydictionary)
 
+
 def imagepage(request):
-    return render (request, 'imagepage.html')    
+    return render(request, 'imagepage.html')
+
 
 def imagepage2(request):
-    return render (request, 'imagepage2.html')    
+    return render(request, 'imagepage2.html')
+
 
 def imagepage3(request):
-    return render (request, 'imagepage3.html')  
+    return render(request, 'imagepage3.html')
+
 
 def imagepage4(request):
-    return render (request, 'imagepage4.html')   
+    return render(request, 'imagepage4.html')
+
 
 def imagepage5(request, imagename):
     myimagename = str(imagename)
@@ -63,15 +75,21 @@ def imagepage5(request, imagename):
         var = True
     elif myimagename == "python":
         var = False
-   
+
     mydictionary = {
-         "var": var
-     }       
-    
+        "var": var
+    }
 
     return render(request, 'imagepage5.html', context=mydictionary)
+
+
 def myform(request):
-    return render(request, 'myform.html')    
+    return render(request, 'myform.html')
+
 
 def submitmyform(request):
-    return render(request, 'submitmyform.html')    
+    mydictionary = {
+        "method" : request.method
+    }
+
+    return JsonResponse(mydictionary)
