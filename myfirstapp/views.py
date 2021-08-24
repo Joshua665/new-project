9dda5729ba4645f772c9b254190260d860ca3c0d
West Joshua Dein <west7828@run.edu.ng>
2021-08-23 20:22:02 -1200

    commit message

--- myfirstapp/views.py
+++ myfirstapp/views.py
@@ -1,124 +1,123 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse,JsonResponse
 from .forms import *
-
 # Create your views here.
 
 
 def myfunctioncall(request):
-    return HttpResponse("Hello World!")
-
+    return HttpResponse("Hello World")
 
 def myfunctionabout(request):
     return HttpResponse("About Response")
 
-
-def add(request, a, b):
+def add(request,a,b):
     return HttpResponse(a+b)
 
-
-def intro(request, name, age):
+def intro(request,name,age):
     mydictionary = {
-        "name": name,
-        "age": age
+        "name" : name,
+        "age" : age
     }
     return JsonResponse(mydictionary)
 
-
 def myfirstpage(request):
-    return render(request, 'index.html')
-
+    return render(request,'index.html')
 
 def mysecondpage(request):
-    return render(request, 'second.html')
-
+    return render(request,'second.html')
 
 def mythirdpage(request):
-    var = "Hello World!"
-    greeting = "Hello how are you?"
-    fruits = ["apple", "banana", "orange"]
-    num1, num2 = 3, 5
+    var = "hello world"
+    greeting = "hey how are you"
+    fruits = ["apple","mango","banana"]
+    num1 , num2 = 10 , 7
     ans = num1 > num2
-    # print(ans)
+    #print(ans)
     mydictionary = {
-        "var": var,
-        "msg": greeting,
-        "myfruits": fruits,
-        "num1": num1,
-        "num2": num2,
-        "ans": ans
-
+        "var" : var,
+        "msg" : greeting,
+        "myfruits" : fruits,
+        "num1" : num1,
+        "num2" : num2,
+        "ans" : ans
     }
-    return render(request, 'third.html', context=mydictionary)
-
+    return render(request,'third.html',context=mydictionary)
 
 def imagepage(request):
-    return render(request, 'imagepage.html')
+    return render(request,'imagepage.html')
 
 
 def imagepage2(request):
-    return render(request, 'imagepage2.html')
-
+    return render(request,'imagepage2.html')
 
 def imagepage3(request):
-    return render(request, 'imagepage3.html')
-
+    return render(request,'imagepage3.html')
 
 def imagepage4(request):
-    return render(request, 'imagepage4.html')
+    return render(request,'imagepage4.html')
 
-
-def imagepage5(request, imagename):
+def imagepage5(request,imagename):
     myimagename = str(imagename)
     myimagename = myimagename.lower()
     print(myimagename)
     if myimagename == "django":
-        var = True
+        var=True
     elif myimagename == "python":
-        var = False
-
-    mydictionary = {
-        "var": var
+        var=False
+    mydictionary ={
+        "var":var
     }
-
-    return render(request, 'imagepage5.html', context=mydictionary)
-
+    return render(request,'imagepage5.html',context=mydictionary)
 
 def myform(request):
-    return render(request, 'myform.html')
-
+    return render(request,'myform.html')
 
 def submitmyform(request):
     mydictionary = {
-        "var1": request.POST['mytext'],
-        "var2": request.POST['mytextarea'],
-        "method": request.method
+        "var1" : request.POST['mytext'],
+        "var2" : request.POST['mytextarea'],
+        "method" : request.method
     }
-
     return JsonResponse(mydictionary)
 
 def myform2(request):
-    if request.method == 'POST':
-        form = FeedbackForm(request.POST) 
-        if form.is_valid():  
+    if request.method == "POST":
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
             title = request.POST['title']
             subject = request.POST['subject']
+            email = request.POST['email']
             mydictionary = {
                 "form" : FeedbackForm()
             }
-            mydictionary['success'] = True
-            mydictionary['successmsg'] = "Form submitted"
+            errorflag = False
+            Errors = []
+            if title != title.upper():
+                errorflag = True
+                errormsg = "Title should be in Capital"
+                Errors.append(errormsg)
+            import re
+            regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+            if not re.search(regex,email):
+                errorflag = True
+                errormsg = "Not a Valid Email address"
+                Errors.append(errormsg)
+            if errorflag != True:
+                mydictionary["success"] = True
+                mydictionary["successmsg"] = "Form Submitted"
+            mydictionary["error"] = errorflag
+            mydictionary["errors"] = Errors
+            print(mydictionary)
             return render(request,'myform2.html',context=mydictionary)
-        else:
-            mydictionary = {
-                "forms": form
-            }   
-        return render(request,'myform2.html',context=mydictionary)
-             
-    elif request.method == 'GET':
-        form = FeedbackForm()
+
+
+
+
+
+
+    elif request.method == "GET":
+        form = FeedbackForm() # FeedbackForm(None)
         mydictionary = {
-            "forms": form
+            "form" : form
         }
-        
         return render(request,'myform2.html',context=mydictionary)
